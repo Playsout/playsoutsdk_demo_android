fd0498db25c8d697562365ed8a1693334d8f49cf
AI&Games <lkbtboy@gmail.com>
2025-11-11 14:26:00 +0800

    add invoke ad ID
    add invoke ad ID

--- app/src/main/java/com/playsout/sdkdemo/DemoApplication.java
+++ app/src/main/java/com/playsout/sdkdemo/DemoApplication.java
@@ -2,19 +2,31 @@ package com.playsout.sdkdemo;
 
 import android.app.Application;
 
+import java.util.HashMap;
+
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.engine.FlutterEngineCache;
 import io.flutter.embedding.engine.dart.DartExecutor;
+import io.flutter.plugin.common.MethodChannel;
 
 public class DemoApplication extends Application {
+    private static final String CHANNEL = "com.playsout.minigames";
     @Override
     public void onCreate() {
         super.onCreate();
-        // Instance FlutterEngine ，cache
+
+	// Instance FlutterEngine ，cache        
         FlutterEngine flutterEngine = new FlutterEngine(this);
         flutterEngine.getNavigationChannel().setInitialRoute("/home?channel=playsout&sdkkey=eyJ2ZXIiOiJ2MSIsImNoYW5uZWwiOiJwbGF5c291dCIsInBhY2thZ2VuYW1lIjoiIiwiZXhwIjoxNzYwOTY3ODIwfS5zaWc"); //update if sdkkey expire,look log
 
         flutterEngine.getDartExecutor().executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault());
+
+        MethodChannel channel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
+        HashMap<String, Object> arguments = new HashMap<>();
+        arguments.put("appAdId", "ca-app-pub-3940256099942544/1712485313"); // change them by your admob ad ID
+        arguments.put("gameAdId", "ca-app-pub-3940256099942544/1712485313");
+        channel.invokeMethod("init",arguments);
+
         FlutterEngineCache.getInstance().put(CacheId.PLAYSOUT_ENGINE_ID, flutterEngine);
     }
 }
