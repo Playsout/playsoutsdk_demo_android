aa2969c5a6bfde35aa281f0748d656cde2635249
tablilixian <273678582@qq.com>
2026-01-22 17:55:02 +0800

    update key

--- app/src/main/java/com/playsout/sdkdemo/DemoApplication.java
+++ app/src/main/java/com/playsout/sdkdemo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication extends Application {
 
 	// Instance FlutterEngine ，cache        
         FlutterEngine flutterEngine = new FlutterEngine(this);
-        flutterEngine.getNavigationChannel().setInitialRoute("/home?channel=playsout&sdkkey=eyJ2ZXIiOiJ2MSIsImNoYW5uZWwiOiJwbGF5c291dCIsInBhY2thZ2VuYW1lIjoiIiwiZXhwIjoxNzY3NzcyNjk1fQ==3M"); //update if sdkkey expire,look log
+        flutterEngine.getNavigationChannel().setInitialRoute("/home?channel=playsout&sdkkey=eyJ2ZXIiOiJ2MSIsImNoYW5uZWwiOiJwbGF5c291dCIsInBhY2thZ2VuYW1lIjoiY29tLnBsYXlzb3V0LnNka2RlbW8iLCJleHAiOjE3NzE2NjY4MzV93Q"); //update if sdkkey expire,look log
 
         flutterEngine.getDartExecutor().executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault());
 
